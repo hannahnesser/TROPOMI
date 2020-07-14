95921b0f19b7857bd5d46307e5c5240bdc20dd0c
Hannah Nesser <hnesser@g.harvard.edu>
2020-07-14 10:48:38 -0400

    Adjusted run script for TROPOMI operator to more appropriately request resources

--- process/run_Compare.sh
+++ process/run_Compare.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
 #SBATCH -p huce_intel
-#SBATCH -n 4      # cores requested
+#SBATCH -n 2      # cores requested
 #SBATCH -N 1      # nodes requested
-#SBATCH --mem=4000  # memory in Mb
+#SBATCH --mem=2000  # memory in Mb
 #SBATCH -o outfile  # send stdout to outfile
 #SBATCH -e errfile  # send stderr to errfile
-#SBATCH -t 4:00:00  # time requested in hour:minute:second
+#SBATCH -t 0-05:00  # time requested in hour:minute:second
 #SBATCH --mail-type=END
 
 # Use modules to set the software environment
