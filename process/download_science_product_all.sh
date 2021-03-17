41111f0ab2bbfb60bb3aff678fcd0cd077b9633a
Hannah Nesser <hnesser@g.harvard.edu>
2021-03-17 12:35:26 -0400

    Changes to update science product downloads and make runnable from home directory (maybe a mistake)

--- process/download_science_product_all.sh
+++ process/download_science_product_all.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-#list=(seq -f "%02g" 5 7)
-list=("01" "06" "09" "11")
+download_dir="/n/holyscratch01/jacob_lab/hnesser/TROPOMI/downloads_new/raw"
+
+list=("10")
+
+#for i in `seq -f "%02g" 1 15`
+
 for i in ${list[@]}
 do
-sbatch ./download_data.sh $i
+    sbatch ./download_science_product.sh $i $download_dir
+    sleep 30s
 done
