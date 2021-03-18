5ca3f6e9a987dc8bb2894755136ee14588a1131b
Hannah Nesser <hnesser@g.harvard.edu>
2021-03-18 14:05:38 -0400

    Changed list of pages and directory where scripts are run from

--- process/download_science_product_all.sh
+++ process/download_science_product_all.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-download_dir="/n/holyscratch01/jacob_lab/hnesser/TROPOMI/downloads_new/raw"
+download_dir="/n/holyscratch01/jacob_lab/hnesser/TROPOMI/downloads_new"
+cp ./download_science_product.sh ${download_dir}
+cd ${download_dir}
 
-list=("10")
+#for i in `seq -f "%02g" 0 15`
 
-#for i in `seq -f "%02g" 1 15`
 
+list=("07" "11")
 for i in ${list[@]}
 do
-    sbatch ./download_science_product.sh $i $download_dir
+    sbatch download_science_product.sh $i
     sleep 30s
 done
