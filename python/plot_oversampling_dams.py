10e9dbc9aa23cc35972fb7a66aa2147cd4f69539
Hannah Nesser <hnesser@g.harvard.edu>
2021-11-23 10:24:52 -0600

    Bug fix for latex

--- python/plot_oversampling_dams.py
+++ python/plot_oversampling_dams.py
@@ -13,7 +13,7 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 from cartopy import feature as cf
-sys.path.append('.')
+sys.path.append('/n/home04/hnesser/TROPOMI/python')
 import format_plots as fp
 import config as config
 
@@ -29,6 +29,7 @@ import shapefile as shp
 
 # Tell matplotlib not to look for an X-window
 environ['QT_QPA_PLATFORM']='offscreen'
+rcParams['text.usetex'] = False
 
 colors = plt.cm.get_cmap('inferno', lut=8)
 
@@ -93,6 +94,10 @@ if __name__ == '__main__':
     REGION = str(sys.argv[2].split(',')[0])
     DATA_DIR = join(DATA_DIR, REGION)
 
+    # DATA_DIR = '/n/seasasfs02/hnesser/TROPOMI/oversampling_output_csvs_14_14/dams/'
+    # REGION = 'harsha'
+    # DATA_DIR = join(DATA_DIR, REGION)
+
     region_dict = {'itaipu' : 'Itaipu',
                    'tucurui' : 'Tucurui',
                    'robertbourassa' : 'Robert-Bourassa (La Grande-2)',
@@ -116,6 +121,8 @@ if __name__ == '__main__':
 
     #latlim = np.array(sys.argv[2].split(',')[1:3]).astype(float)
     #lonlim = np.array(sys.argv[2].split(',')[3:5]).astype(float)
+    # lat = 39.02424
+    # lon = -84.131436
     lat = float(sys.argv[2].split(',')[5])
     lon = float(sys.argv[2].split(',')[6])
     latlim = np.array([lat - 1, lat + 1]).round(2)
